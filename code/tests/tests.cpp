129ddb76765940ea4fe4a934b57d03c2396c6dc5
eddiebiv <eddiebrenmark@gmail.com>
2023-10-25 17:22:47 -0500

    tests

---
+++ code/tests/tests.cpp
@@ -0,0 +1,60 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "../src/pagerank.h"
+
+#include <sstream>
+#include <vector>
+
+/*
+ * Reads in the dataset and searches for the data on row 1
+ */
+TEST_CASE("Case 1: Site A's Rank", "[weight=5]")
+{
+	std::vector<double> expected;
+    expected.push_back(1/3);
+    expected.push_back(1/3);
+    expected.push_back(1/3);
+
+    std::string infile = "../../data/webgraph_variations.csv";
+    std::string search = "1";
+    WebGraph(infile, search);
+    std::vector<double> out = getPageRank();
+
+    REQUIRE(expected == out);
+}
+
+/*
+ * Reads in the dataset and searches for the data on row 2
+ */
+TEST_CASE("Case 2: Site B's Rank", "[weight=5]")
+{
+	std::vector<double> expected;
+    expected.push_back(0.464);
+    expected.push_back(0.4865);
+    expected.push_back(0.05);
+
+    std::string infile = "../../data/webgraph_variations.csv";
+    std::string search = "2";
+    WebGraph(infile, search);
+    std::vector<double> out = getPageRank();
+
+    REQUIRE(expected == out);
+}
+
+/*
+ * Reads in the dataset and searches for the data on row 3
+ */
+TEST_CASE("Case 3: Site C's Rank", "[weight=5]")
+{
+	std::vector<double> expected;
+    expected.push_back(0.433);
+    expected.push_back(1/3);
+    expected.push_back(0.234);
+
+    std::string infile = "../../data/webgraph_variations.csv";
+    std::string search = "3";
+    WebGraph(infile, search);
+    std::vector<double> out = getPageRank();
+
+    REQUIRE(expected == out);
+}
