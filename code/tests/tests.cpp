3b06d742ac512505c80da6a1029149cf9999f2a9
Ahmed Shafiuddin <112600024+ahmed23shaf@users.noreply.github.com>
2023-10-25 23:19:07 -0500

    Update tests.cpp

--- code/tests/tests.cpp
+++ code/tests/tests.cpp
@@ -16,7 +16,11 @@ TEST_CASE("Case 1: 3 disconnected components", "[weight=5]")
 
     std::string infile = "../../data/webgraph_variations.csv";
     int search = 1;
+	
     WebGraph graph(infile, search);
+    graph.convertToMarkov();
+    graph.computePageRank();
+	
     std::vector<double> out = graph.getPageRank();
 
     REQUIRE(expected == out);
@@ -34,7 +38,11 @@ TEST_CASE("Case 2: 3 connected components; C is only outgoing", "[weight=5]")
 
     std::string infile = "../../data/webgraph_variations.csv";
     int search = 2;
+	
     WebGraph graph(infile, search);
+    graph.convertToMarkov();
+    graph.computePageRank();
+	
     std::vector<double> out = graph.getPageRank();
 
     REQUIRE(expected == out);
@@ -52,7 +60,11 @@ TEST_CASE("Case 3: 3 connected components", "[weight=5]")
 
     std::string infile = "../../data/webgraph_variations.csv";
     int search = 3;
+	
     WebGraph graph(infile, search);
+    graph.convertToMarkov();
+    graph.computePageRank();
+	
     std::vector<double> out = graph.getPageRank();
 
     REQUIRE(expected == out);
