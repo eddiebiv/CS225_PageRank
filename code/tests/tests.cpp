75c1064cbf01b7aaa3cd605a5394ffcc5e496062
Ahmed Shafiuddin <112600024+ahmed23shaf@users.noreply.github.com>
2023-10-25 20:28:24 -0500

    Update tests.cpp

--- code/tests/tests.cpp
+++ code/tests/tests.cpp
@@ -9,7 +9,7 @@
  */
 TEST_CASE("Case 1: 3 disconnected components", "[weight=5]")
 {
-	std::vector<double> expected;
+    std::vector<double> expected;
     expected.push_back(1/3);
     expected.push_back(1/3);
     expected.push_back(1/3);
@@ -17,7 +17,7 @@ TEST_CASE("Case 1: 3 disconnected components", "[weight=5]")
     std::string infile = "../../data/webgraph_variations.csv";
     int search = 1;
     WebGraph graph(infile, search);
-    std::vector<double> out = getPageRank();
+    std::vector<double> out = graph.getPageRank();
 
     REQUIRE(expected == out);
 }
